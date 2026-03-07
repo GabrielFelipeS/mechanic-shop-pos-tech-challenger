7937e52bcb21f9ca6847e7b1bf786b87dd79d319
eddie-marley <marley_eddie@hotmail.com>
2026-03-07 11:10:03 -0300

    feat: Initial project structure with Docker and basic configurations.

---
+++ src/main/java/org/project/postechchallengemechanicshop/PosTechChallengeMechanicShopApplication.java
@@ -0,0 +1,13 @@
+package org.project.postechchallengemechanicshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTechChallengeMechanicShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosTechChallengeMechanicShopApplication.class, args);
+    }
+
+}
