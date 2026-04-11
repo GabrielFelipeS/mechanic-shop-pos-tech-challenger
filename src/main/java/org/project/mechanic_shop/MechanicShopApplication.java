31aaf8cc73d26d9d0a1439d158849f65feb54106
kaizen <belebraz16@gmail.com>
2026-04-11 16:47:52 -0300

    :recycle: refactor: move annotation enableJpaAuditing to Configurarion JPA

--- src/main/java/org/project/mechanic_shop/MechanicShopApplication.java
+++ src/main/java/org/project/mechanic_shop/MechanicShopApplication.java
@@ -3,13 +3,9 @@ package org.project.mechanic_shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class MechanicShopApplication {
 
     public static void main(String[] args) {
