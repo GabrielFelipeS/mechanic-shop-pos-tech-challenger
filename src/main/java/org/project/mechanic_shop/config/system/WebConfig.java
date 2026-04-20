6415f67fd81a0b5fb148b1d5829f561bb986ec9e
eddie-marley <marley_eddie@hotmail.com>
2026-04-20 16:53:42 -0300

    refactor: created classes to system config

---
+++ src/main/java/org/project/mechanic_shop/config/system/WebConfig.java
@@ -0,0 +1,9 @@
+package org.project.mechanic_shop.config.system;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
