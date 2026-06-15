66692d010c15c9671649b29db55b72315497707c
eddie-marley <marley_eddie@hotmail.com>
2026-06-15 13:27:40 -0300

    feat: Features created as requested by Postech level 2. Improve clean Arch

--- src/main/java/org/project/mechanic_shop/application/listeners/StockNotificationListener.java
+++ src/main/java/org/project/mechanic_shop/application/listeners/StockNotificationListener.java
@@ -3,10 +3,10 @@ package org.project.mechanic_shop.application.listeners;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.project.mechanic_shop.application.services.UserService;
 import org.project.mechanic_shop.domain.events.OutOfStockEvent;
 import org.project.mechanic_shop.domain.entities.user.User;
-import org.project.mechanic_shop.domain.enums.UserRoleEnum; // Importação corrigida para o seu Enum
-import org.project.mechanic_shop.infrastructure.repositories.UserRepository;
+import org.project.mechanic_shop.domain.enums.UserRoleEnum;
 import org.project.mechanic_shop.infrastructure.http.EmailService;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 public class StockNotificationListener {
 
 	private final EmailService emailService;
-	private final UserRepository userRepository;
+	private final UserService userService;
 
 	@Async
 	@EventListener
@@ -38,7 +38,7 @@ public class StockNotificationListener {
 
 		List<String> targetRoles = List.of(UserRoleEnum.WAREHOUSE_CLERK.name(), UserRoleEnum.BUYER.name());
 
-		List<User> notificationTargets = userRepository.findByRoleIn(targetRoles);
+		List<User> notificationTargets = userService.findByRoles(targetRoles);
 
 		if (notificationTargets.isEmpty()) {
 			log.error("No users found with roles {} to receive the stock alert!", targetRoles);
