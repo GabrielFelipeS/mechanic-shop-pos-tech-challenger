82a89785820b13daa1ff925741bbb1642e0c20f6
eddie-marley <marley_eddie@hotmail.com>
2026-07-07 23:31:54 -0300

    fix: fixed bugs

--- src/main/java/org/project/mechanic_shop/shared/config/security/SecurityConfigurations.java
+++ src/main/java/org/project/mechanic_shop/shared/config/security/SecurityConfigurations.java
@@ -33,7 +33,7 @@ public class SecurityConfigurations {
 				authorize
 					.requestMatchers(HttpMethod.POST, "/api/auth/login")
 					.permitAll()
-					.requestMatchers(HttpMethod.GET, "/api/v1/service-orders/budget-approval")
+					.requestMatchers(HttpMethod.GET, "/api/service-orders/budget-approval")
 					.permitAll()
 					.requestMatchers(HttpMethod.GET, "/actuator/health")
 					.permitAll()
